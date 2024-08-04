ba07f855844882ea41687db164ae65bdc419e079
Alex Weisman <ajweisman@gmail.com>
2024-08-03 20:53:17 -0700

    middlewares 2.1

--- testingMiddleware.js
+++ testingMiddleware.js
@@ -1,8 +1,13 @@
-// testingMiddleware.js
 export const handleTesting = (req, res, next) => {
-    if (req.body.event.type != "create_pulse") {
-        console.log('req.body.event', req.body.event)
+    if (!req.body || !req.body.event || !req.body.event.type) {
+        console.error('Invalid request structure:', req.body);
+        return res.status(400).send({ error: 'Invalid request structure' });
+    }
+
+    if (req.body.event.type !== "create_pulse") {
+        console.log('req.body.event', req.body.event);
         return res.status(200).send(req.body);
     }
-    next(); // Pass the request to the next middleware/route handler if no challenge key is found
+
+    next(); // Pass the request to the next middleware/route handler if the event type is "create_pulse"
 };
